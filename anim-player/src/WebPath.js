80490abe3086af8c647cc488aeffea4293188087
Gabriel Sta Ana <gistaana8755@gmail.com>
2024-01-31 17:48:54 -0500

    firebase implementation omplete, deleting last page still in progress

--- anim-player/src/WebPath.js
+++ anim-player/src/WebPath.js
@@ -4,6 +4,7 @@ import LandingPage from "./Auth/LandingPage";
 import Register from "./Auth/Register";
 import MainPage from "./Pages/MainPage";
 import RotatingImages from './Pages/RotatingImages';
+import RiV2 from './Pages/RiV2'
 
 
 function WebPath() {
@@ -11,11 +12,10 @@ function WebPath() {
   
     <Routes> 
   
-      <Route path="/" element={<LandingPage />} /> 
+      <Route path="/" element={<RiV2 />} /> 
       <Route path="/Register" element={<Register />} /> 
       <Route path="/MainPage" element={<MainPage />} /> 
-      <Route path="/RotatingImages" element={<RotatingImages/>}/>
-      
+    
     </Routes>
 
 
